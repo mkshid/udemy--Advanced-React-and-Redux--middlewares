a90969ac5d30ee07b3810f5b3d4e104c718a4b41
M. Hassan Khurshid <mkshid@gmail.com>
2017-07-06 23:28:17 +0200

    Add some css to make it look better

--- src/components/user_list.js
+++ src/components/user_list.js
@@ -22,7 +22,7 @@ class UserList extends Component {
 
     render() {
         return (
-            <div>
+            <div className='user-list'>
               {this.props.users.map(this.renderUser)}
             </div>
         );
