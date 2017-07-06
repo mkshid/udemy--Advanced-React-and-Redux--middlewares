0b47150c147d2a10372ceeba8a0ea8f26055afce
M. Hassan Khurshid <mkshid@gmail.com>
2017-07-06 23:47:54 +0200

    Start writing first middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,11 @@
 import { FETCH_USERS } from './types';
+import axios from 'axios';
+
 
 export function fetchUsers() {
-
+    const request = axios.get('https://jsonplaceholder.typicode.com/users');
     return {
         type: FETCH_USERS,
-        payload: [
-            {name: 'Hassan'},
-            {name: 'Iris'},
-            {name: 'Artem'}
-        ]
+        payload: request
     };
 }
---
+++ src/middlewares/async.js
@@ -0,0 +1,8 @@
+export default function ({dispatch}){
+    return next => action => {
+        console.log(action);
+        next(action);
+    };
+}
+
+
